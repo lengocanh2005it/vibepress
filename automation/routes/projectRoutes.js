3922d780506599d52171e45448c7befd92c1a22a
KeriaKH <151910742+KeriaKH@users.noreply.github.com>
2026-05-05 08:46:37 +0700

    refactor: remove uploadTheme function and related wpressExtractor utility

--- automation/routes/projectRoutes.js
+++ automation/routes/projectRoutes.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const upload = require('../middlewares/uploadMiddleware');
 const { requireAuth } = require('../middlewares/authMiddleware');
-const { createProject, getProjectById, uploadTheme, registerWpSite, getToken, syncComplete, triggerDbSyncEndpoint, getReposByEmail, getCommitsByRepo, getWpSitePages, proxyWpPage, proxyWpAsset, notifyContentChange, notifyCommentChange, getDBinfoBySiteId, getSqlDumpTables, getSqlDumpRows, getSqlDumpFullTable, getSqlDumpAll, createSiteDb } = require('../controllers/projectController');
+const { createProject, getProjectById, registerWpSite, getToken, syncComplete, triggerDbSyncEndpoint, getReposByEmail, getCommitsByRepo, getWpSitePages, proxyWpPage, proxyWpAsset, notifyContentChange, notifyCommentChange, getDBinfoBySiteId, getSqlDumpTables, getSqlDumpRows, getSqlDumpFullTable, getSqlDumpAll, createSiteDb } = require('../controllers/projectController');
 
 const router = express.Router();
 
 router.post('/create-project', createProject);
 router.get('/project/:projectId', getProjectById);
-router.post('/upload-theme', upload.single('wpressFile'), uploadTheme);
 router.post('/wp/register', registerWpSite);
 router.post('/wp/get-token', getToken);
 router.post('/wp/sync-complete', syncComplete);
